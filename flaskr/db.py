110a1623bd2fa0f2d726e5305c4c9b80c0d81148
Cristyle Garrard <29622724+CristylePGarrard@users.noreply.github.com>
2021-02-09 15:42:54 -0700

    Update db.py

--- flaskr/db.py
+++ flaskr/db.py
@@ -5,15 +5,19 @@ from flask iport current_app, g
 from flask.cli import with_appcontext
 
 def get_db():
-  if 'db' not in g:
-    g.db = sqlite3.connect(
-      current_app.config['DATABASE'],
-      detect_types=sqlite3.PARSE_DECLTYPES
-    )
-    g.db.row_factory = sqlite3.Row
-  return g.db
+    if 'db' not in g:
+      g.db = sqlite3.connect(
+        current_app.config['DATABASE'],
+        detect_types=sqlite3.PARSE_DECLTYPES
+      )
+      g.db.row_factory = sqlite3.Row
+    return g.db
   
 
 def close_db(e=None0:
-  db = g.pop('db', None)
+    db = g.pop('db', None)
+             
   
+    if db is not None:
+        db.close()
+             
